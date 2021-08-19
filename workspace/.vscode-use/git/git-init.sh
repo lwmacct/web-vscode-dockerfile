48a50dd20f913c6c57f32ea692681d5750b24d1c
lwmacct <lwmacct@163.com>
2021-08-20 01:56:06 +0800

    init

--- workspace/.vscode-use/git/git-init.sh
+++ workspace/.vscode-use/git/git-init.sh
@@ -9,8 +9,8 @@ git init
 git remote add origin git@gitee.com:lwmacct/web-vscode-dockerfile.git
 git remote set-url --add origin git@github.com:lwmacct/web-vscode-dockerfile.git
 
+git config lfs.https://github.com/lwmacct/web-vscode-dockerfile.git/info/lfs.locksverify false
+
 # git lfs install
 # git lfs track "*.tar.gz"
 # git add /config/.gitattributes
-
-git config lfs.https://github.com/lwmacct/web-vscode-dockerfile.git/info/lfs.locksverify false
